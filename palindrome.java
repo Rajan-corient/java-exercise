fed317de0be69997f80991ae22e5738a10db428a
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2022-09-26 20:52:49 +0530

    static method added for palindrome

--- palindrome.java
+++ palindrome.java
@@ -1,6 +1,7 @@
-class Palindrome {
-    public static void main(String[] args) {
-        int num = 3553, reversedNum = 0, remainder;
+class Main {
+
+    public static boolean checkPalindrome(int num) { 
+        int reversedNum = 0, remainder;
 
         // store the number to originalNum
         int originalNum = num;
@@ -15,10 +16,21 @@ class Palindrome {
 
         // check if reversedNum and originalNum are equal
         if (originalNum == reversedNum) {
-            System.out.println(originalNum + " is Palindrome.");
+            return true;
         }
         else {
-            System.out.println(originalNum + " is not Palindrome.");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int num = 3553;
+        // check if reversedNum and originalNum are equal
+        if (checkPalindrome(num)) {
+            System.out.println(num + " is Palindrome.");
+        }
+        else {
+            System.out.println(num + " is not Palindrome.");
         }
     }
 }
